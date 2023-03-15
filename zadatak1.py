ea75416afab65f5478e4b2218563a19a614801a9
MatejMijok <78339573+MatejMijok@users.noreply.github.com>
2023-03-15 12:32:20 +0100

    Update zadatak1.py

--- zadatak1.py
+++ zadatak1.py
@@ -1,6 +1,8 @@
 radniSati = int(input("Unesite broj radnih sati: "))
 satnica = float(input("Unesite satnicu u eurima: "))
 
-ukupno = radniSati * satnica
+def total_euro():
+  ukupno = radniSati * satnica
+  return ukupno
 
-print("Ukupno iznosi: " + str(ukupno)) 
+print("Ukupno iznosi: " + str(total_euro())) 
